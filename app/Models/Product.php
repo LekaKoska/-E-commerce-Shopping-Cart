da6ca3dc603e453d861b5dcfefb801ccea609c8d
LekaKoska <159024082+LekaKoska@users.noreply.github.com>
2025-12-27 16:34:37 +0100

    * Creating Products model and migration * Setting default length string

---
+++ app/Models/Product.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    const TABLE = 'products';
+    protected $table = self::TABLE;
+    protected $fillable = ['name', 'price', 'stock_quantity'];
+}
