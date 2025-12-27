101df929750ce721275e041d1dc1e354c5e355ac
LekaKoska <159024082+LekaKoska@users.noreply.github.com>
2025-12-27 17:16:12 +0100

    * Using trait for factory * Adding new route and editing blade

---
+++ resources/views/products/products.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Document</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @livewireScripts
+</head>
+<body>
+<livewire:products/>
+
+@livewireStyles
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,8 @@ Route::get('/', function () {
     return view('welcome');
 })->name('home');
 
+Route::get('products', fn() => view('products.products'))->name('products');
+
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
